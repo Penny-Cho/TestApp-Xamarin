c0ba2d5a8f01af71d502b708528c9f41a452871a
조정은 <pencho119@gmail.com>
2021-08-18 10:49:10 +0900

    added listview to profilepage with linq

--- TestApp/TestApp/ProfilePage.xaml.cs
+++ TestApp/TestApp/ProfilePage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TestApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +17,45 @@ namespace TestApp
         {
             InitializeComponent();
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = conn.Table<Post>().ToList();
+                //var categories = (from p in postTable orderby p.CategoryId select p.CategoryName).Distinct().ToList();
+
+                var categories = postTable.OrderBy(p => p.CategoryId).Select(p => p.CategoryName).Distinct().ToList();
+
+                //fitering data into dictionary
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+
+                foreach (var category in categories)
+                {
+                    //var count = (from post in postTable where post.CategoryName == category select post).ToList().Count;
+
+                    var count = postTable.Where(p => p.CategoryName == category).ToList().Count;
+
+                    if (category != null)
+                    {
+                        categoriesCount.Add(category, count);
+                    }
+                    else
+                    {
+                        categoriesCount.Add("Uncategorized", count);
+                    }
+
+
+                }
+
+                categoriesListView.ItemsSource = categoriesCount;
+
+                postCountLabel.Text = postTable.Count.ToString();
+            }
+
+        }
+
     }
 }
